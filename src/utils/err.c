1c44b33e22095fc4d73a2a364ffaaa0da6628f3b
Timothee "TTimo" Besset <ttimo@ttimo.net>
2014-08-13 16:31:44 -0500

    prepare for routing log output to other facilities

--- src/utils/err.c
+++ src/utils/err.c
@@ -186,3 +186,10 @@ void nn_win_error (int err, char *buf, size_t bufsize)
 
 #endif
 
+void nn_err_log ( const char * fmt, ... ) {
+	va_list argp;
+	va_start (argp, fmt);
+	vfprintf (stderr, fmt, argp);
+	va_end (argp);
+	fflush (stderr);
+}
