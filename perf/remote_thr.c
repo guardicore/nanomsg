5adb2078e5f736851a65d7bf27676120ab25e41f
Timothee "TTimo" Besset <ttimo@ttimo.net>
2014-10-08 10:14:08 -0500

    looking at passing a callback to manage this better

--- perf/remote_thr.c
+++ perf/remote_thr.c
@@ -29,14 +29,6 @@
 #include <assert.h>
 #include <stdarg.h>
 
-void nn_err_log ( const char * fmt, ... ) {
-	va_list argp;
-	va_start (argp, fmt);
-	vfprintf (stderr, fmt, argp);
-	va_end (argp);
-	fflush (stderr);
-}
-
 int main (int argc, char *argv [])
 {
     const char *connect_to;
